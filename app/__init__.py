0ffb4597d42e8e6083b9f392394efaa2942d5874
Ishant <ishantgaming371@gmail.com>
2026-03-02 11:07:25 +0530

    clean folder

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Vehicle Automation Microservice - Phase 1 Scaffold"""
--- app/allocation/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Allocation module for vehicle trip generation."""
-from app.allocation.allocation_engine import AllocationEngine, Order, Trip
-
-__all__ = ['AllocationEngine', 'Order', 'Trip']
